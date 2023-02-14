79e936b2bbaba2c61f79372435a4cee390a87c4b
Alexey7272 <106250222+Alexey7272@users.noreply.github.com>
2023-02-15 01:03:26 +0200

    do save contacts in local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,12 +8,7 @@ import shortid from "shortid";
 export default class App extends Component {
 
   state = {
-    contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -64,6 +59,21 @@ export default class App extends Component {
           contacts: contacts.filter(contact => contact.id !== id)
         }
       })
+  };
+
+  componentDidMount = () => {
+      const contacts = localStorage.getItem('contacts');
+      const parsedContacts = JSON.parse(contacts);
+
+      if( parsedContacts ) {
+        this.setState({contacts: parsedContacts});
+      };
+  };
+
+  componentDidUpdate = ( prevProps, prevState) => {
+    if ( this.state.contacts !== prevState.contacts) {
+       localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+    };
   };
 
   render () {
